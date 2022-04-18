3f891521436f33a40d96ebcf9b56a86c3ecd5cfe
imashu <debug.ashu@gmail.com>
2022-04-18 15:28:46 +0530

    checkbox_bottom_sheet

---
+++ android/app/src/main/kotlin/flutter/OnGraph/checkbox_in_bottom_sheet/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutter.OnGraph.checkbox_in_bottom_sheet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  bool? _value1 = false;
+  bool? _value2 = false;
+
+  void _showBottom() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (BuildContext context,
+              StateSetter setState /*You can rename this!*/) {
+            return Container(
+              padding: const EdgeInsets.all(15.0),
+              child: LimitedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: CheckboxListTile(
+                        value: _value1,
+                        onChanged: (bool? value) =>
+                            setState(() => _value1 = value!),
+                        title: Text('Ashutosh'),
+                        controlAffinity: ListTileControlAffinity.leading,
+                        //subtitle: Text('Subtitle'),
+                        secondary: Icon(Icons.people),
+                        activeColor: Colors.red,
+                      ),
+                    ),
+                    Expanded(
+                      child: CheckboxListTile(
+                        value: _value2,
+                        onChanged: (bool? value) =>
+                            setState(() => _value2 = value!),
+                        title: Text('Ajit Sir'),
+                        controlAffinity: ListTileControlAffinity.leading,
+                        //subtitle: Text('Subtitle'),
+                        secondary: Icon(Icons.people),
+                        activeColor: Colors.red,
+                      ),
+                    ),
+                    Flexible(
+                      child: ElevatedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('Close'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          });
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom sheet'),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text('Select a person:'),
+                ElevatedButton(
+                    onPressed: _showBottom, child: Text('Show names')),
+              ],
+            ),
+          )),
+    );
+  }
+}
